8525c195490cdba2a2db095c79a264ae7e04f3b4
PrinceGill2 <princesinghgill2007@gmail.com>
2024-08-04 13:41:57 -0700

    add a change density feature

--- javascrik.js
+++ javascrik.js
@@ -1,17 +1,72 @@
-let squares = prompt("how many squares per side?");
-let area = squares*squares;
-let size = 480/squares;
 const gridContainer = document.getElementById("GRIDCONTAINER");
+let childrenDivs;
+let squares;
+let area;
+let size;
+function ask() { 
+    squares = prompt("how many squares per side?");
+    area = squares*squares;
+    size = 480/squares;
 
-for (let i = 0; i < area; i++) { 
-    gridContainer.appendChild(document.createElement("div"));
+    
+}
+
+function addChildren() { 
+    for (let i = 0; i < area; i++) { 
+        gridContainer.appendChild(document.createElement("div"));
+    
+    }
 
 }
 
-const childrenDivs = gridContainer.querySelectorAll("div");
+function activateChildren() { 
+    childrenDivs = gridContainer.querySelectorAll("div");
+    for (let i = 0; i < area; i++) { 
+        childrenDivs[i].style.height = (size + "px");
+        childrenDivs[i].style.width = (size + "px")
+        childrenDivs[i].addEventListener("mouseover", () => { 
+            childrenDivs[i].style.backgroundColor = "#808080"
+        })
+    }
 
-for (let i = 0; i < area; i++) { 
-    childrenDivs[i].textContent = "I";
-    childrenDivs[i].style.height = (size + "px");
-    childrenDivs[i].style.width = (size + "px")
 }
+
+function ridChildren()  {
+    gridContainer.replaceChildren();
+    
+
+}
+
+
+    
+
+
+
+function createGrid() { 
+    ask();
+    addChildren();
+    activateChildren();
+    
+}
+
+createGrid();
+
+
+
+
+
+
+
+
+
+
+
+
+const button = document.getElementById("button");
+button.addEventListener("click", () => { 
+    ridChildren();
+    ask();
+    addChildren();
+    activateChildren();
+});
+
